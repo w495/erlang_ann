6e9f7442f0a13c4f38e5857f971559cd2f0ec9c0
w-495 <w@w-495.ru>
2013-01-07 19:37:37 +0400

    add docs

---
+++ priv/doc/Erlang_and_Neural_Networks_files/index.php
@@ -0,0 +1,9 @@
+
+/* generated javascript */
+var skin = 'trapexit.org';
+var stylepath = '/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Trapexit.org.js (deprecated; migrate to Common.js!) */
